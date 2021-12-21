00a4b12f399f01f214209200198c6ff9649ec64f
0xbono <iam@sonjim.in>
2021-12-21 22:01:33 +0900

    feat(main): webpack-hmr 활성화 및 CORS 등 기본적인 기능 구성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+declare const module: any;
+
+async function create() {
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+
+  await app.listen(3000, () => {
+    this.logger('http://localhost:3000');
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
-bootstrap();
+
+create();
